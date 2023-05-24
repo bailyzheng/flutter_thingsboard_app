d3122e4e4f1ad9aabda4461c4160890423f50214
stones Zheng <eezhenglei@126.com>
2023-05-24 08:22:46 +0800

    fix

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://localhost:8080';
+  static final thingsBoardApiEndpoint = 'http://192.168.3.29:8080';
   static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
